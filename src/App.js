4a3b03c8cd9184cce613b34d270fda3c1a98a511
debangi <debangiofficial@gmail.com>
2022-05-10 21:57:56 +0530

    added sidebar header and sidebar search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>React app</h1>
+    <div className='app'>
+      <div className='app__body'>
+        <Sidebar />
+        {/* Chat */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Avatar from '@mui/material/Avatar';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from '@mui/material/IconButton';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar__header'>
+        <Avatar alt='' />
+        <div className='sidebar__headerRight'>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className='sidebar__search'>
+        <div className='sidebar__searchContainer'>
+          <SearchOutlined />
+          <input placeholder='Search or start a new chat' type='text' />
+        </div>
+      </div>
+      <div className='sidebar__chats'></div>
+    </div>
+  );
+};
+
+export default Sidebar;
