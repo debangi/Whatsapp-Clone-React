edda3e405e958a4bf35d7263a12c990ec7f68eb7
debangi <debangiofficial@gmail.com>
2022-05-10 20:36:59 +0530

    cleanup complete

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA3SD1WjvUmqPTa66-fpKz7gmolr2iy0CQ',
+  authDomain: 'whatsapp-clone-b01d2.firebaseapp.com',
+  projectId: 'whatsapp-clone-b01d2',
+  storageBucket: 'whatsapp-clone-b01d2.appspot.com',
+  messagingSenderId: '499999005954',
+  appId: '1:499999005954:web:4542cad6f6b7573e22761b',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
